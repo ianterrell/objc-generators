2baaedf43ddddbbed30456ff86ffc2fadde86c98
Ian Terrell <ian.terrell@gmail.com>
2009-03-15 21:47:10 -0400

    added examples

---
+++ examples/myapp.rb
@@ -0,0 +1,22 @@
+require '../sqlitepo-gen'
+
+class Objects < SqlitePOGenerator
+  project_name "MyApp"
+  directory "myapp"
+  author "Ian Terrell"
+  
+  persistent_object :user do |user|
+    user.string :username
+    user.integer :user_id
+    user.belongs_to :group
+    user.belongs_to :group, :name => :another_group
+  end
+  
+  persistent_object :group do |group|
+    group.string :title
+    group.string :description
+    group.integer :group_id
+  end
+  
+  
+end
